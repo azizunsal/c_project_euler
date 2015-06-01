682fccc36a842567497d0564066a9b29b8d2ec60
Aziz Unsal <unsal.aziz@gmail.com>
2015-06-01 11:03:37 +0300

    Renamed the source file. Used symbolic constant.

--- p001/multiples_of_3_and_5.c
+++ p001/multiples_of_3_and_5.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define LIMIT 1000
+
 int main() {
 
 	int sum = 0;
-	for (int i=1; i<1000; i++ ) {
+
+	for (int i=1; i< LIMIT; i++ ) {
 		if (i % 3 == 0 || i % 5 == 0) {
 			sum += i;
 		}
+
 	}
 	printf("Sum of multiples of 3 or 5 below 1000 is %d\n", sum);
 
